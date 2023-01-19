edd7062dfc701407466e61ad9487c43f62fcdd78
Lucas Seeley <69803592+LucasSeeley@users.noreply.github.com>
2023-01-18 19:38:58 -0500

    cookies

--- public/app.js
+++ public/app.js
@@ -3,3 +3,26 @@ $(document).ready(function(){
     const app = firebase.app();
     console.log(app);
 });
+
+function setCookie(name, value, days){
+    const date = new Date();
+    date.setTime(date.getTime() + (days*24*60*60*1000));
+    let expiration = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value};${expiration};path=/`;
+}
+
+function getCookie(cname){
+    let name = `${cname}=`;
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookies = decodedCookie.split(';');
+
+    for(let i = 0; i < cookies.length; i++){
+        let cookie = cookies[i];
+        while(cookie.charAt(0) == ' '){
+            cookie = cookie.substring(1);
+        }
+        if(cookie.indexOf(name) === 0){
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+}
