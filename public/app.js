56e9d2aa2b4c145609431cec155f6dc1e298009b
Lucas Seeley <69803592+LucasSeeley@users.noreply.github.com>
2023-01-26 10:06:38 -0500

    firebase functions

--- public/app.js
+++ public/app.js
@@ -1,8 +1,35 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 $(document).ready(function(){
 
-    const app = firebase.app();
+    const firebaseConfig = {
+        // ...   
+    };
+
+    const app = initializeApp(firebaseConfig);
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth(app);
+
     console.log(app);
+
+    if(getCookie("user") != null){
+        signInUser(user.email, user.password);
+    }
 });
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      
+      const uid = user.uid;
+      $("#sign-in-nav").hide();
+    } else {
+      $("#sign-in-nav").show();
+    }
+  });
 
 function setCookie(name, value, days){
     const date = new Date();
@@ -25,4 +52,31 @@ function getCookie(cname){
             return cookie.substring(name.length, cookie.length);
         }
     }
+}
+
+
+function registerUser(email, password){
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    setCookie("user", user, 3);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+function signInUser(email, password){
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 }
